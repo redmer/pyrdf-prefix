4756f1032d37c297c0846157f7dd47b819f0837e
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2024-10-01 14:03:09 +0200

    ci(pyproject.toml): add version and commitizen

--- src/pyrdf_prefix/__init__.py
+++ src/pyrdf_prefix/__init__.py
@@ -1,4 +1,7 @@
+from importlib.metadata import version
+
 from .namespace import PredefinedNamespace, Prefix
 from .predefined import *
 
+__version__ = version("pyrdf_prefix")
 __all__ = ["Prefix"]
